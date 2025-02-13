89a9fbfcfaad0cbb9200b869a3f46717f4f8fe10
ghat0tkach <awesomevikram3@gmail.com>
2025-02-13 07:12:31 +0530

    feat:added landing page
    Signed-off-by: ghat0tkach <awesomevikram3@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import { AnimatedBackground } from "@/components/ui/AnimatedBackground";
-import { RegistrationForm } from "@/components/registration/RegistrationForm";
-import { initialData, registrationSteps } from "@/lib/constants";
+import BackgroundPaths from "@/components/ui/BackgroundPaths";
 
-export default function Home() {
-
-
-  return (
-    <div className="relative min-h-screen w-full overflow-hidden">
-      <AnimatedBackground />
-      <RegistrationForm
-        steps={registrationSteps}
-        initialData={initialData}
-      />
-    </div>
-  );
+export default function Page() {
+  return <BackgroundPaths />
 }
